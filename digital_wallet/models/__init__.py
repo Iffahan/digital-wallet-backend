54865a727b757e941bb6c3bd2749b811deb2975e
อิฟฟาฮาน สุขสุวรรณ <jackcopsluncher@gmail.com>
2024-08-18 17:21:48 +0700

    change models and add model user

--- digital_wallet/models/__init__.py
+++ digital_wallet/models/__init__.py
@@ -1,29 +1,44 @@
 from typing import Optional
 
 from sqlmodel import Field, SQLModel, create_engine, Session, select
+from sqlmodel.ext.asyncio.session import AsyncSession
 
-#from . import merchants, items, wallets, transactions
-from . import merchants, items
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+
+from . import items
+from . import merchants
+from . import users
 
 from .items import *
 from .merchants import *
-#from .wallets import *
-#from .transactions import * 
+from .users import *
 
 
 connect_args = {}
 
-engine = create_engine(
-    "postgresql+pg8000://admin:admin@localhost/db_wallet",
-    echo=True,
-    connect_args=connect_args,
-)
+engine = None
 
 
-def init_db():
-    SQLModel.metadata.create_all(engine)
+def init_db(settings):
+    global engine
+
+    engine = create_async_engine(
+        settings.SQLDB_URL,
+        echo=True,
+        future=True,
+        connect_args=connect_args,
+    )
 
 
-def get_session():
-    with Session(engine) as session:
+async def create_all():
+    async with engine.begin() as conn:
+        #await conn.run_sync(SQLModel.metadata.drop_all)
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    async with async_session() as session:
         yield session
