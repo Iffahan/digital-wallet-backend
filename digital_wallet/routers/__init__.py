d47761a9f47b6b120204b946a3506473699ef5df
อิฟฟาฮาน สุขสุวรรณ <jackcopsluncher@gmail.com>
2024-08-25 23:01:54 +0700

    add test and fix init

--- digital_wallet/routers/__init__.py
+++ digital_wallet/routers/__init__.py
@@ -4,7 +4,9 @@ from . import merchants
 from . import authentication
 from . import wallets
 from . import transactions
+from . import root
 def init_router(app):
+    app.include_router(root.router)
     app.include_router(users.router)
     app.include_router(authentication.router)
     app.include_router(items.router)
---
+++ tests/test_merchant.py
@@ -0,0 +1,12 @@
+import pytest
+from httpx import AsyncClient
+from digital_wallet import models
+
+@pytest.mark.asyncio
+async def test_no_permission_create_merchants(
+    client: AsyncClient, user1: models.DBUser
+):
+    payload = {"name": "merchants", "user_id": user1.id}
+    response = await client.post("/merchants", json=payload)
+
+    assert response.status_code == 401
